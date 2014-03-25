4253590c7f3b015b7e82f811bac15d5b3dc7171c
Jonas Thuresson <thuresson.j@gmail.com>
2014-03-25 14:31:33 +0100

    First commit

---
+++ src/garage/BicycleGarageManager.java
@@ -0,0 +1,30 @@
+package garage;
+import interfaces.*;
+
+public class BicycleGarageManager{
+
+	public void registerHardwareDrivers(BarcodePrinter printer,
+			ElectronicLock entryLock, ElectronicLock exitLock,
+			PinCodeTerminal terminal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void entryBarcode(String bicycleID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void exitBarcode(String bicycleID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void entryCharacter(char c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
