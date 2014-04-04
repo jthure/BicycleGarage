d462ae5bd9a69af5b7d8edda637f8a77f18640e6
Jonas Thuresson <thuresson.j@gmail.com>
2014-04-04 09:21:01 +0200

    user-bike first touch

--- src/garage/Bicycle.java
+++ src/garage/Bicycle.java
@@ -1,9 +1,26 @@
 package garage;
 
 public class Bicycle {
+	private String id;
+	private boolean inGarage;
 
-	public Bicycle(String s) {
-		// TODO Auto-generated constructor stub
+	public Bicycle(String id) {
+		this.id = id;
+		inGarage = false;
+	}
+	
+	public String getId(){
+		return id;
+	}
+	public void setID(String id){
+		this.id = id;
+	}
+	
+	public boolean isInGarage(){
+		return inGarage;
+	}
+	public void setInGarage(boolean inGarage){
+		this.inGarage = inGarage;
 	}
 
 }
--- src/garage/User.java
+++ src/garage/User.java
@@ -1,5 +1,40 @@
 package garage;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class User {
 
+	private List<Bicycle> bicycleList;
+
+	private String name;
+	private String pin;
+	
+	private static final int PIN_LENGTH = 6;
+	
+	public User(String name, String pin){
+		if(pin.length()!=PIN_LENGTH){
+			throw new IllegalArgumentException("Pin code is of wrong length");
+		}
+		this.name = name;
+		this.pin = pin;
+		bicycleList = new LinkedList<>();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPin() {
+		return pin;
+	}
+
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+	
 }
